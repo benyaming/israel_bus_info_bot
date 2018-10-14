93347bc8f1eaef260b9b2b546cfd3272165c75ea
Benyomin Ginzburg <benyomin.94@gmail.com>
2018-10-15 00:58:57 +0300

    update env variables

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ from os import environ
 
 
 TOKEN = environ.get('TOKEN')
-BOT_HOST = ''
-BOT_PORT = ''
+BOT_HOST = environ.get('BOT_HOST')
+BOT_PORT = environ.get('BOT_PORT')
 
 IS_SERVER = environ.get('IS_SERVER')
