be1eaabca4da2c023fcae16cd92d17c83bcf9f57
Benyamin Ginzburg <benyomin.94@gmail.com>
2021-07-30 08:49:59 +0300

    Fix typo

--- bus_bot/texts.py
+++ bus_bot/texts.py
@@ -1,5 +1,5 @@
 start_command = 'Hi! Send me location or a stop number!'
-help_command = 'Send me your location, and I will find all public transport stops, located near to you. Or' \
+help_command = 'Send me your location, and I will find all public transport stops, located near to you. Or ' \
                'just write me your stop\'s number, and I will send you ' \
                'arrival times of nearest buses. The message with times will ' \
                'updating each few seconds for 15 minutes or until you send ' \
